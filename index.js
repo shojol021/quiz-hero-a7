eb20220a69600cb6b35700859bd15aa710a03f0a
shojol021 <shojol021@gmail.com>
2023-03-16 23:53:28 +0600

    gave a final check, removed all console.log

--- index.js
+++ index.js
@@ -40,29 +40,25 @@ startQuiz.addEventListener("click", () => {
     }
     counter.innerText = counterNum;
     counterNum--;
-  }, 100);
+  }, 1000);
 });
 
 // All quiz data fetched from json
 const loadQuiz = async () => {
   const res = await fetch("./data/quiz.json");
   const data = await res.json();
-  // console.log(data);
   quizData = data;
   displayQuiz(data);
 };
 
 // Displaying quiz on quiz page
 const displayQuiz = (data) => {
-  // data.forEach(q => console.log(q))
-  // console.log(data)
   if (!data) {
     quizContainer.innerHTML = "";
     return;
   }
 
   data.forEach((quiz, i) => {
-    console.log(quiz);
     quizContainer.innerHTML += `<div class="m-3 py-3 px-4 shadow-sm rounded">
   <div class="flex items-center">
     <div class="h-8 w-8 bg-green-300 rounded-full flex justify-center items-center text-green-800 mr-3">
@@ -113,7 +109,6 @@ document.querySelector("#submit").addEventListener("click", () => {
 
   // data setting on local storage and getting data from local storage
   let storage = JSON.parse(localStorage.getItem("results"));
-  console.log(localStorage.getItem("results"))
   if (storage) {
     localStorage.setItem(
       "results",
