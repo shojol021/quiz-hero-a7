d98e1d67be19f81ebf077775ab4d248bf9f79b99
shojol021 <shojol021@gmail.com>
2023-03-16 08:12:50 -0700

    fixed show quistions and local storage issue

--- index.js
+++ index.js
@@ -40,31 +40,35 @@ startQuiz.addEventListener("click", () => {
     }
     counter.innerText = counterNum;
     counterNum--;
-  }, 1000);
+  }, 100);
 });
 
 // All quiz data fetched from json
 const loadQuiz = async () => {
   const res = await fetch("./data/quiz.json");
-  const data = await res.json;
+  const data = await res.json();
+  // console.log(data);
   quizData = data;
   displayQuiz(data);
 };
 
 // Displaying quiz on quiz page
 const displayQuiz = (data) => {
+  // data.forEach(q => console.log(q))
+  // console.log(data)
   if (!data) {
     quizContainer.innerHTML = "";
     return;
   }
 
   data.forEach((quiz, i) => {
+    console.log(quiz.question);
     quizContainer.innerHTML += `<div class="m-3 py-3 px-4 shadow-sm rounded">
   <div class="flex items-center">
     <div class="h-8 w-8 bg-green-300 rounded-full flex justify-center items-center text-green-800 mr-3">
       ${i + 1}
     </div>
-    <p class="text-gray-800 text-sm">${quiz.quetion}</p>
+    <p class="text-gray-800 text-sm">${quiz.question}</p>
   </div>
   <div class="grid grid-cols-2 gap-4 mt-5">
     ${displayQuizOptions(quiz.options, i)}
@@ -74,7 +78,7 @@ const displayQuiz = (data) => {
 };
 
 // EventListener for quiz submit button
-document.querySelector("#submit").addEventlistener("click", () => {
+document.querySelector("#submit").addEventListener("click", () => {
   if (answers.length < 6) {
     return;
   }
@@ -108,7 +112,8 @@ document.querySelector("#submit").addEventlistener("click", () => {
   }
 
   // data setting on local storage and getting data from local storage
-  let storage = JSON.parse(localStorage.getItem("result"));
+  let storage = JSON.parse(localStorage.getItem("results"));
+  console.log(localStorage.getItem("results"))
   if (storage) {
     localStorage.setItem(
       "results",
