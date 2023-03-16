dd4db0339c1a504a7c79536a3bf2d7d78ae62a89
shojol021 <shojol021@gmail.com>
2023-03-16 23:14:20 +0600

    updated the blog html ans for the 2 functions operration

--- index.js
+++ index.js
@@ -62,7 +62,7 @@ const displayQuiz = (data) => {
   }
 
   data.forEach((quiz, i) => {
-    console.log(quiz.question);
+    console.log(quiz);
     quizContainer.innerHTML += `<div class="m-3 py-3 px-4 shadow-sm rounded">
   <div class="flex items-center">
     <div class="h-8 w-8 bg-green-300 rounded-full flex justify-center items-center text-green-800 mr-3">
